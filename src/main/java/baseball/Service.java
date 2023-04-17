9a5990a28995c63bc16477f367c4d1e6720e1f13
simba <shim9597@gmail.com>
2023-04-17 19:48:48 +0900

    feat: 볼과 스트라이크의 개수를 구하는 기능 구현

---
+++ src/main/java/baseball/Service.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import java.util.List;
+
+public class Service {
+
+    Computer computer;
+
+    public void setComputer(List<Integer> randomNumbers) {
+        computer = new Computer(randomNumbers);
+    }
+
+    public int countStrike(List<Integer> userNumbers) {
+        int strike = computer.countSameNumberInSameIndex(userNumbers);
+        return strike;
+    }
+
+    public int countBall (List<Integer> userNumbers) {
+        int countSameNumber = computer.countSameNumber(userNumbers);
+
+        int strike = computer.countSameNumberInSameIndex(userNumbers);
+        int ball = countSameNumber - strike;
+        return ball;
+    }
+}
