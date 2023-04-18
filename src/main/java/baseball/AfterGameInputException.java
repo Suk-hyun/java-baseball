72e29464680a7dc3214c3646270b44a41750c85e
simba <shim9597@gmail.com>
2023-04-18 22:46:25 +0900

    feat: 게임 후 입력값 예외 기능 구현
    입력값이 1 또는 2 인지 확인한다.

---
+++ src/main/java/baseball/AfterGameInputException.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import java.util.Objects;
+
+public class AfterGameInputException {
+
+    public static void checkException(String input) {
+        if (isIllegalInput(input)) {
+            throw new IllegalArgumentException("입력값은 1 또는 2 만 가능합니다.");
+        }
+
+    }
+
+    private static boolean isIllegalInput(String input) {
+        return !Objects.equals(input, "1") && !Objects.equals(input, "2");
+    }
+}
