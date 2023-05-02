4a3f7a93830837eacab05a275bafaa85a9cf8ed1
simba <shim9597@gmail.com>
2023-05-02 22:36:21 +0900

    refactor: 예외처리 기능에 throws 문 추가

--- src/main/java/baseball/exceptions/AfterGameInputException.java
+++ src/main/java/baseball/exceptions/AfterGameInputException.java
@@ -1,10 +1,10 @@
-package baseball;
+package baseball.exceptions;
 
 import java.util.Objects;
 
 public class AfterGameInputException {
 
-    public static void checkException(String input) {
+    public static void checkException(String input) throws IllegalArgumentException{
         if (isIllegalInput(input)) {
             throw new IllegalArgumentException("입력값은 1 또는 2 만 가능합니다.");
         }
