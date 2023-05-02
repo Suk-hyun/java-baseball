0ce3d84c9efe6c7aaee11067a04af58c734d6e8b
simba <shim9597@gmail.com>
2023-05-02 22:37:01 +0900

    feat: 메인 메서드에 컨트롤러 코드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.controller.Controller;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView.gameStartMsg();
+        Controller controller = new Controller();
+        controller.setGame();
+        controller.playGame();
     }
 }
